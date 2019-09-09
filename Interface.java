ab9d884d64b42af63b9a5c40dc47d57b26a7a2f2
ErickdaMata <erickdamata@gmail.com>
2019-09-09 20:04:02 -0300

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Interface {
+
+	public static void main(String[] args) 
+	{
+		boolean continuar;
+		String resposta;
+		Scanner sc = new Scanner(System.in);
+		MotorDeInferencia motorDeInferencia = new MotorDeInferencia();
+		
+		do 
+		{
+
+			motorDeInferencia.iniciar();
+			
+			do {
+				System.out.println("INTERFACE | Encontrou resposta = " + motorDeInferencia.encontrouResposta());
+				System.out.println(motorDeInferencia.proximaPergunta() + "(S-Sim | N-Não)");
+				//Le a resposta
+				resposta = sc.next();
+				motorDeInferencia.respondeParametroAtual(resposta);
+			}while (!motorDeInferencia.encontrouResposta());
+			
+			System.out.println(motorDeInferencia.getRespostaFinal());
+			
+			//Realiza a pergunta
+			System.out.println("Continuar (S-Sim | N-Não)");
+			//Le a resposta
+			resposta = sc.next();
+			
+			if(resposta.equals("S"))
+			{
+				continuar = true;
+			}
+			else
+			{
+				continuar = false;
+			}
+			
+		} while (continuar);
+		
+		
+
+	}
+}
